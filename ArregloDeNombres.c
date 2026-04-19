709e86c94e99b810ffb01d110a6f98f6a42e93cc
Lenis <lenis99lg@gmail.com>
2026-04-19 11:52:47 -0300

    Ejercicio 2 Parte 1

---
+++ ArregloDeNombres.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void MostrarPersonas(char *v[],int cant);
+int main(){
+    char buff[50]; //arreglo donde reside temporalmente el nombre
+    char *V[5]; // arreglos de 5 elementos
+
+    //pido al usuario los 5 nombres a ingresar
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Ingrese el nombre:");
+        scanf("%s",buff);//temporalmente se almacena en buff
+        int tama= strlen(buff); //calculo el tamanio del nombre
+        
+        //reservo memoria para el tamanio del nombre
+        V[i]=(char *)malloc(sizeof(char)* (tama +1));
+        strcpy(V[i], buff); //copia lo que esta en el buff al arreglo V
+    }
+    MostrarPersonas(V,5);
+    //liberar memoria
+    for (int i = 0; i < 5; i++)
+    {
+        free(V[i]);
+    }
+    
+    return 0;
+}
+void MostrarPersonas(char *v[],int cant){
+    printf("\n--Lista de Personas---\n");
+    for (int i = 0; i < cant; i++)
+    {
+        printf("%d: %s\n",i+1,v[i]);
+    }
+    
+}
