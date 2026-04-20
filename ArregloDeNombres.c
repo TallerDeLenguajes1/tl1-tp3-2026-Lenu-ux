fe1fd88012a3b24d66734e1432a70e7f6b482d46
Lenis <lenis99lg@gmail.com>
2026-04-20 20:26:09 -0300

    Interfaz de usuario y busqueda

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 void MostrarPersonas(char *v[],int cant);
+int BusarNombrePorPalabra(char *v[],int cant, char *paClave);
+void BuscarNombrePorId(int ID,char *v[],int cant);
 int main(){
     char buff[50]; //arreglo donde reside temporalmente el nombre
     char *V[5]; // arreglos de 5 elementos
@@ -18,6 +20,37 @@ int main(){
         strcpy(V[i], buff); //copia lo que esta en el buff al arreglo V
     }
     MostrarPersonas(V,5);
+//interfaz para el usuario
+    int opcion=0;
+    int idBuscar;
+    char palabra[50];
+
+    printf("---Menu de Busqueda----\n");
+    printf("1.Buscar por ID\n2.Buscar por palabra clave\n Seleccione: ");
+    scanf("%d",&opcion);
+    switch (opcion)
+    {
+    case 1:
+        printf("Ingrese el ID a buscar: ");
+        scanf("%d",&idBuscar);
+        BuscarNombrePorId(idBuscar,V,5);
+        break;
+    case 2:
+        getchar();
+        printf("Ingrese la palabra clave: ");
+        gets(palabra);
+        int indice=BusarNombrePorPalabra(V,5,palabra);
+        if (indice != -1)
+        {
+            printf("Coincidencia encontrada: %s (ID:%d)\n",V[indice],indice+1);
+        }else
+        {
+            printf("No hubo coincidencias para %s\n",palabra);
+        }
+    break;
+    default:
+        break;
+    }
     //liberar memoria
     for (int i = 0; i < 5; i++)
     {
@@ -55,7 +88,7 @@ void BuscarNombrePorId(int ID,char *v[],int cant){
             printf("Id %d: %s\n",ID,v[ID]);
 
         }else{
-            printf("no se encontró el valor buscado\n");
+            printf("no se encontro el valor buscado\n");
         }
     
 }
